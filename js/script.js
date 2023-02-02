e1f614f1f6c046d2057254a71465e2cb2ab5b712
asplundoo <oliver@asplund.net>
2023-02-02 12:25:09 +0100

    v4

--- js/script.js
+++ js/script.js
@@ -53,3 +53,32 @@ var responsiveSlider = function() {
     window.onload = function() {
     responsiveSlider();  
     }
+
+
+
+/* --- Dropdown ------------------------- */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+window.onclick = function(event) {
+    if (!event.target.matches(".dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns [i];
+            if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+  
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+    
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+.show {display: block;}
