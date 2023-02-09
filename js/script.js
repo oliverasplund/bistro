264dbf5883b6d529f3447aea8eb092928dcc15f4
asplundoo <oliver@asplund.net>
2023-02-09 11:55:20 +0100

    Co-authored-by: wigrenmarcus <wigrenmarcus@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,55 +1,23 @@
-var responsiveSlider = function() {
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links a");
 
-    var slider = document.getElementById("slider");
-    var sliderWidth = slider.offsetWidth;
-    var slideList = document.getElementById("slideWrap");
-    var count = 1;
-    var items = slideList.querySelectorAll("li").length;
-    var prev = document.getElementById("prev");
-    var next = document.getElementById("next");
-    
-    window.addEventListener('resize', function() {
-      sliderWidth = slider.offsetWidth;
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s `;
+      }
     });
-    
-    var prevSlide = function() {
-      if(count > 1) {
-        count = count - 2;
-        slideList.style.left = "-" + count * sliderWidth + "px";
-        count++;
-      }
-      else if(count = 1) {
-        count = items - 1;
-        slideList.style.left = "-" + count * sliderWidth + "px";
-        count++;
-      }
-    };
-    
-    var nextSlide = function() {
-      if(count < items) {
-        slideList.style.left = "-" + count * sliderWidth + "px";
-        count++;
-      }
-      else if(count = items) {
-        slideList.style.left = "0px";
-        count = 1;
-      }
-    };
-    
-    next.addEventListener("click", function() {
-      nextSlide();
-    });
-    
-    prev.addEventListener("click", function() {
-      prevSlide();
-    });
-    
-    setInterval(function() {
-      nextSlide()
-    }, 5000);
-    
-    };
-    
-    window.onload = function() {
-    responsiveSlider();  
-    }
+    burger.classList.toggle("toggle");
+  });
+  //
+};
+
+navSlide();
